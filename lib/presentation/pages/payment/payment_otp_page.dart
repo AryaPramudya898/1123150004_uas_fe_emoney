91fb500472480fa549a11497bd657e881dd385ed
AryaPramudya898 <aryapramudya610@gmail.com>
2026-06-21 21:57:42 +0700

    feat: sedikit merapihkan kode

--- lib/presentation/pages/payment/payment_otp_page.dart
+++ lib/presentation/pages/payment/payment_otp_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import '../../../core/constants/app_constants.dart';
+import '../../../core/services/deeplink_callback_service.dart';
 import '../../../core/theme/app_colors.dart';
 import '../../../core/utils/currency_formatter.dart';
 import '../../blocs/payment/payment_bloc.dart';
@@ -57,6 +58,16 @@ class _PaymentOtpPageState extends State<PaymentOtpPage> {
       listener: (context, state) {
         if (state is PaymentTransferSuccess) {
           final result = state.result;
+
+          // Kirim callback sukses ke merchant jika ada callbackUrl
+          final callbackUrl = flow['callbackUrl'] as String?;
+          if (callbackUrl != null && callbackUrl.isNotEmpty) {
+            DeeplinkCallbackService.notifySuccess(
+              callbackUrl: callbackUrl,
+              reference: flow['reference'] as String?,
+              transactionId: result.transactionId,
+            );
+          }
           context.go('/success', extra: {
             'title': 'Transfer berhasil',
             'subtitle': result.description,
